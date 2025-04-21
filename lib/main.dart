19b11056c80b49c0af3ab867e43e58552f57970e
Paulo Henrique <148291338+PauloHenriqque@users.noreply.github.com>
2025-04-21 14:25:41 -0300

    Adicionando a opção do usuário criar as transações e deixando a tela scrollable

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:expenses/components/transaction_user.dart';
-import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(const ExpensesApp());
@@ -15,33 +14,21 @@ class ExpensesApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
 
-  final _transactions = [
-    Transaction(
-      id: 't1',
-      title: 'Novo tênis de corrida',
-      value: 310.76,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Conta de Luz',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-  ];
   MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Despesas Pessoais')),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Card(child: Text('Gráfico'), color: Colors.blue, elevation: 5),
-          TransactionUser(),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Card(child: Text('Gráfico'), color: Colors.blue, elevation: 5),
+            TransactionUser(),
+          ],
+        ),
       ),
     );
   }
